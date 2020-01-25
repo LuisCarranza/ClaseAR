bf7278c03d7782eade08ba3d36d47fb0af49574c
LuisCarranza <luiscarranza1995@gmail.com>
2020-01-24 19:19:07 -0600

    First Commit

---
+++ Assets/Scritps/TeddyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeddyController : MonoBehaviour
+{
+    private DefaultTrackableEventHandler dte;
+    public Animator oso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dte = GetComponent<DefaultTrackableEventHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dte.isActive)
+        {
+            oso.SetTrigger("active");
+        }
+    }
+}
