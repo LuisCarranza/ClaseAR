3b5c5f5159e1054938f1078df28ffab2bb931702
Luis Carranza <luisal3195@yahoo.com.mx>
2020-03-18 19:08:38 -0600

    Last progress before excercise. Backup

--- Assets/Scritps/TeddyController.cs
+++ Assets/Scritps/TeddyController.cs
@@ -6,11 +6,21 @@ public class TeddyController : MonoBehaviour
 {
     private DefaultTrackableEventHandler dte;
     public Animator oso;
+    public Ray ray;
+
     // Start is called before the first frame update
     void Start()
     {
         dte = GetComponent<DefaultTrackableEventHandler>();
     }
+
+    /*
+    TODO: change raycaster to a new camera script and make functions here to
+     send the triggers
+     https://gamedevelopment.tutsplus.com/tutorials/introduction-to-vuforia-on-unity-for-creating-augmented-reality-applications--cms-27693
+     
+    */
+
 
     // Update is called once per frame
     void Update()
@@ -18,6 +28,26 @@ public class TeddyController : MonoBehaviour
         if (dte.isActive)
         {
             oso.SetTrigger("active");
+        }
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hitInfo;
+                Debug.Log("Generating ray: " + ray.ToString());
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    Debug.Log("Hit something!");
+                    Debug.Log("Hit Info: " + hitInfo.ToString());
+                    // var rig = hitInfo.collider.GetComponent<GameObject>();
+                    // Debug.Log("Hit tag: " + rig.tag);
+                    // if (rig.GetComponent<Animator>() != null)
+                    // {
+                    //     oso.SetTrigger("dead");
+                    // }
+                }
+            }
         }
     }
 }
